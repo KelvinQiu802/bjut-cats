8bd0ed5586d10d6a6403578dde5d15576aa548c0
KelvinQiu <kelvinqiu802@outlook.com>
2023-06-22 20:22:31 +0800

    feat: add navigation bar

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,25 @@
 export default defineAppConfig({
-  pages: [
-    'pages/index/index'
-  ],
+  pages: ['pages/index/index', 'pages/about/about'],
+  tabBar: {
+    list: [
+      {
+        iconPath: './icon/guide.png',
+        selectedIconPath: './icon/guide_selected.png',
+        pagePath: 'pages/index/index',
+        text: '图鉴',
+      },
+      {
+        iconPath: './icon/about.png',
+        selectedIconPath: './icon/about_selected.png',
+        pagePath: 'pages/about/about',
+        text: '关于',
+      },
+    ],
+  },
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
-  }
-})
+    navigationBarTextStyle: 'black',
+  },
+});
--- src/pages/index/index.config.ts
+++ src/pages/index/index.config.ts
@@ -1,3 +1,3 @@
 export default definePageConfig({
-  navigationBarTitleText: '首页'
-})
+  navigationBarTitleText: 'BJUT小动物观赏协会',
+});
