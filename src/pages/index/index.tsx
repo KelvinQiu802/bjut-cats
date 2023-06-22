47d37024c99cda3ff13a2b0945b031ca25c553ee
KelvinQiu <kelvinqiu802@outlook.com>
2023-06-22 23:54:32 +0800

    feat: filter cats when state change

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { View, Text } from '@tarojs/components';
 import { useLoad } from '@tarojs/taro';
 import dataString from '../../../data/dataString';
@@ -9,11 +9,19 @@ import StateBar from './components/StateBar';
 export default function Index() {
   const [cats, setCats] = useState<Cat[]>([]);
   const [state, setState] = useState<State>('在校');
+  const allCats: Cat[] = useMemo( () => JSON.parse(dataString), [dataString])
 
   useLoad(() => {
     console.log('Index Page loaded.');
     setCats(JSON.parse(dataString));
   });
+
+  useEffect(() => {
+    setCats(() => {
+      const filtered = allCats.filter((cat) => cat.State == state);
+      return filtered;
+    });
+  }, [state]);
 
   return (
     <View className='content'>
