14cf840743009123d641d998936fc0270ad16156
KelvinQiu <kelvinqiu802@outlook.com>
2023-06-23 12:41:12 +0800

    feat: add taro ui

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { PropsWithChildren } from 'react'
 import { useLaunch } from '@tarojs/taro'
+import 'taro-ui/dist/style/index.scss'
 import './app.css'
 
 function App({ children }: PropsWithChildren) {
