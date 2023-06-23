3a6f9a99ee41ef4e8dcde54d93fffd809c7abc96
KelvinQiu <kelvinqiu802@outlook.com>
2023-06-23 23:30:40 +0800

    feat: scroll tab

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -4,14 +4,15 @@ import { useLoad, navigateTo } from '@tarojs/taro';
 import dataString from '../../../data/dataString';
 import style from './index.module.css';
 import CatLink from './components/CatLink';
-import StateBar from './components/StateBar';
+import { AtTabs, AtTabsPane } from 'taro-ui';
 import CampusForm from './components/CampusForm';
 import Search from './components/Search';
 import { AtDivider } from 'taro-ui';
 
 export default function Index() {
   const [cats, setCats] = useState<Cat[]>([]);
-  const [state, setState] = useState<State>('在校');
+  // 0: 在校 1:毕业 2:休学 3:喵星
+  const [state, setState] = useState(0);
   const [campus, setCampus] = useState<Campus>('本部');
   const [searchWord, setSearchWord] = useState('');
   const allCats: Cat[] = useMemo(() => JSON.parse(dataString), [dataString]);
@@ -23,8 +24,7 @@ export default function Index() {
 
   useEffect(() => {
     setCats(() => {
-      let filtered = allCats.filter((cat) => cat.State == state);
-      filtered = filtered.filter((cat) => cat.Campus == campus);
+      let filtered = allCats.filter((cat) => cat.Campus == campus);
       filtered = filtered.filter((cat) =>
         cat.Name.toLowerCase().includes(searchWord.toLowerCase())
       );
@@ -35,7 +35,7 @@ export default function Index() {
       });
       return orderByWeight;
     });
-  }, [state, campus, searchWord]);
+  }, [campus, searchWord]);
 
   const handleNavigate = (cat: Cat) => {
     navigateTo({
@@ -49,19 +49,94 @@ export default function Index() {
         <CampusForm campus={campus} setCampus={setCampus} />
         <Search searchWord={searchWord} setSearchWord={setSearchWord} />
       </View>
-      <StateBar state={state} setState={setState} />
-      {cats.map((cat) => {
-        return (
-          <View
-            key={cat.Name}
-            onClick={() => {
-              handleNavigate(cat);
-            }}
-          >
-            <CatLink cat={cat} />
+      <AtTabs
+        animated={true}
+        current={state}
+        swipeable
+        tabList={[
+          { title: '在校' },
+          { title: '毕业' },
+          { title: '休学' },
+          { title: '喵星' },
+        ]}
+        onClick={(index) => {
+          setState(index);
+        }}
+        className={style.stateTab}
+      >
+        <AtTabsPane current={state} index={0}>
+          <View className={style.catList}>
+            {cats
+              .filter((cat) => cat.State == '在校')
+              .map((cat) => {
+                return (
+                  <View
+                    key={cat.Name}
+                    onClick={() => {
+                      handleNavigate(cat);
+                    }}
+                  >
+                    <CatLink cat={cat} />
+                  </View>
+                );
+              })}
           </View>
-        );
-      })}
+        </AtTabsPane>
+        <AtTabsPane current={state} index={1}>
+          <View className={style.catList}>
+            {cats
+              .filter((cat) => cat.State == '毕业')
+              .map((cat) => {
+                return (
+                  <View
+                    key={cat.Name}
+                    onClick={() => {
+                      handleNavigate(cat);
+                    }}
+                  >
+                    <CatLink cat={cat} />
+                  </View>
+                );
+              })}
+          </View>
+        </AtTabsPane>
+        <AtTabsPane current={state} index={2}>
+          <View className={style.catList}>
+            {cats
+              .filter((cat) => cat.State == '休学')
+              .map((cat) => {
+                return (
+                  <View
+                    key={cat.Name}
+                    onClick={() => {
+                      handleNavigate(cat);
+                    }}
+                  >
+                    <CatLink cat={cat} />
+                  </View>
+                );
+              })}
+          </View>
+        </AtTabsPane>
+        <AtTabsPane current={state} index={3}>
+          <View className={style.catList}>
+            {cats
+              .filter((cat) => cat.State == '喵星')
+              .map((cat) => {
+                return (
+                  <View
+                    key={cat.Name}
+                    onClick={() => {
+                      handleNavigate(cat);
+                    }}
+                  >
+                    <CatLink cat={cat} />
+                  </View>
+                );
+              })}
+          </View>
+        </AtTabsPane>
+      </AtTabs>
       <AtDivider content="没有更多猫猫啦" fontColor="#bbb" />
     </View>
   );
