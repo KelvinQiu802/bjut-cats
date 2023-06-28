869df174ad61fe3584b07c77f047bae82cc1623f
KelvinQiu <kelvinqiu802@outlook.com>
2023-06-28 23:05:28 +0800

    feat: add icon

--- src/app.config.ts
+++ src/app.config.ts
@@ -17,17 +17,11 @@ export default defineAppConfig({
         text: '图鉴',
       },
       {
-        iconPath: './icon/guide.png',
-        selectedIconPath: './icon/guide_selected.png',
+        iconPath: './icon/gallery.png',
+        selectedIconPath: './icon/gallery_selected.png',
         pagePath: 'pages/gallery/gallery',
         text: '相册',
       },
-      // {
-      //   iconPath: './icon/guide.png',
-      //   selectedIconPath: './icon/guide_selected.png',
-      //   pagePath: 'pages/uploadImage/uploadImage',
-      //   text: 'TEST',
-      // },
       {
         iconPath: './icon/about.png',
         selectedIconPath: './icon/about_selected.png',
