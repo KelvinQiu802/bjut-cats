026f71d0a95905a48358a72a9d9e7da7925c1ee5
KelvinQiu <kelvinqiu802@outlook.com>
2023-09-26 14:36:07 +0800

    refactor: fetch articles from server

--- src/pages/articles/articles.config.ts
+++ src/pages/articles/articles.config.ts
@@ -2,4 +2,6 @@ export default definePageConfig({
   navigationBarTitleText: '月亮湖猫屋',
   enableShareAppMessage: true,
   enableShareTimeline: true,
+  enablePullDownRefresh: true,
+  backgroundTextStyle: 'dark',
 });
